81ce62cda81e6a31d6952397893fef061fa37c4e
Robbe.Heirman <robbe.heirman@wolterskluwer.com>
2025-07-18 10:10:07 +0200

    remaining code

--- backend/src/transactions/rest/routes.go
+++ backend/src/transactions/rest/routes.go
@@ -1,6 +1,10 @@
 package rest
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log/slog"
+	"net/http"
+)
 
 type Client struct {
 	// domain domain.AuthService
@@ -10,6 +14,21 @@ func CreateClient() *Client {
 	return &Client{}
 }
 
-func (client *Client) receiveKbcTransactionsCsv(gin *gin.Context) {
+func (client *Client) receiveKbcTransactionsCsv(g *gin.Context) {
+	file, err := g.FormFile("file")
+	if err != nil {
+		slog.Error(err.Error())
+		g.JSON(http.StatusBadRequest, gin.H{"failed to get file %s": err.Error()})
+	}
+
+	// Refactor to seperate
+	fileHeader, err := file.Open()
+	defer func() {
+		localErr := fileHeader.Close()
+		if localErr != nil {
+			err = localErr
+			slog.Error("Problems closing file", "reason", err.Error())
+		}
+	}()
 
 }
